d1d106fac41f54bf4166c6f3ee74c33530561ec9
CL <cl.czayas@gmail.com>
2022-11-30 22:59:07 -0400

    Added questions array

--- index.js
+++ index.js
@@ -3,7 +3,65 @@ const fs = require("fs")
 const inquirer = require("inquirer")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [   
+    
+{   //Title
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+    
+},
+    
+{   //Description
+    type: "input",
+    name: "description",
+    message: "Please enter a description of your project.",
+
+},
+
+{   //Installation
+    type: "input",
+    name: "installation",
+    message: "Please enter the installation instructions.",
+    
+},
+
+{   //Usage
+    type: "input",
+    name: "usage",
+    message: "How is this project used?",
+    
+},
+
+{   //License
+    type: "list",
+    name: "license",
+    message: "Please select a license for this project.",
+    choices: [
+        "GNU AGPLv3",
+        "GNU GPLv3",
+        "GNU LGPLv3",
+        "Apache 2.0",
+        "Boost Software 1.0",
+        "MIT",
+        "Mozilla",
+    ],
+    
+},
+
+{   //GH Username
+    type: "input",
+    name: "userName",
+    message: "What is your GitHub username?",
+    
+},
+
+{   //email
+    type: "input",
+    name: "userEmail",
+    message: "What is your email address?",
+}
+]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
